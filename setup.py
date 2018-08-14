4e98820b478ae18fbe1d2d5f93055ffc6bd974ab
Mohkale <mohkalsin@gmail.com>
2018-08-14 23:25:36 +0100

    Create Setup Files

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.MD', 'r') as File:
+    long_description = File.read() 
+
+setuptools.setup(
+    name='wrap_logger',
+    version='1.0.0',
+    author='Mohkale',
+    auther_email='Mohkalsin@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/MoHKale/Wrap-Logger',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
